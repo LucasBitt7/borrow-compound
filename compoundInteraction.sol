6f4f21cd758f1f11b5bf854355ca9811db8f48fd
LucasBitt7 <lucas.s.bitencourt07@gmail.com>
2022-05-25 20:35:19 -0300

    first commit

---
+++ compoundInteraction.sol
@@ -0,0 +1,72 @@
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "./ComptrollerInterface.sol";
+import "./CTokenInterface.sol";
+
+contract Compound {
+    IERC20 dai;
+    CTokenInterface cDai;
+    IERC20 usdc;
+    CTokenInterface cUsdc;
+    ComptrollerInterface comptroller;
+    address _dai = 0x6A9865aDE2B6207dAAC49f8bCba9705dEB0B0e6D;
+    address _cDai = 0x6D7F0754FFeb405d23C51CE938289d4835bE3b14;
+    address _usdc = 0x4DBCdF9B62e891a7cec5A2568C3F4FAF9E8Abe2b;
+    address _cUsdc = 0x5B281A6DdA0B271e91ae35DE655Ad301C976edb1;
+    address _comptroller = 0x2EAa9D77AE4D8f9cdD9FAAcd44016E746485bddb;
+
+    constructor() {
+        dai = IERC20(_dai);
+        cDai = CTokenInterface(_cDai);
+        usdc = IERC20(_usdc);
+        cUsdc = CTokenInterface(_cUsdc);
+        comptroller = ComptrollerInterface(_comptroller);
+    }
+
+    function deposit(uint _amount) public {
+           
+    dai.transferFrom(msg.sender, address(this), _amount);
+     dai.approve(address(cDai), _amount);
+      require(cDai.mint(_amount) == 0, "mint failed");
+    }
+    function withdraw(uint _amount) public {
+        require(cDai.redeem(_amount) == 0, "failed");
+    }
+    function getDaiBalance() public view returns(uint) {
+        return dai.balanceOf(msg.sender);
+    }
+    function getDaiContractBalance() public view returns(uint) {
+        return dai.balanceOf(address(this));
+    }
+    function borrow(uint _amount) public {
+        address[] memory markets = new address[](1);
+        markets[0] = address(cDai);
+        comptroller.enterMarkets(markets);
+        cUsdc.borrow(_amount);
+    }
+    function payBorrow(uint _amount) public {
+        usdc.approve(address(cUsdc), _amount);
+        cUsdc.repayBorrow(_amount);
+    }
+    function getCDaiBalance() public view returns(uint) {
+        return cDai.balanceOf(address(this));
+    }
+    function getCUSDCBalance() public view returns(uint) {
+        return cUsdc.balanceOf(address(this));
+    }
+      function getInfo() external returns (uint exchangeRate, uint supplyRate) {
+    // Amount of current exchange rate from cToken to underlying
+    exchangeRate = _cDai.exchangeRateCurrent();
+    // Amount added to you supply balance this block
+    supplyRate = _cDai.supplyRatePerBlock();
+  }
+    function getCollateralFactor() external view returns (uint) {
+    (bool isListed, uint colFactor, bool isComped) = comptroller.markets(
+      address(cToken)
+    );
+    return colFactor; // divide by 1e18 to get in %
+  }
+  
+
+}
